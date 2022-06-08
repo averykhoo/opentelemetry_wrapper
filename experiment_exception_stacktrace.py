fd3c99f52bc7fe183a0299c527848661d6b5b648
Avery <averykhoo@gmail.com>
2022-06-08 10:46:54 +0800

    traceback printing

--- experiment_exception_stacktrace.py
+++ experiment_exception_stacktrace.py
@@ -20,15 +20,18 @@ class A:
 if __name__ == '__main__':
     try:
         f(1)
-    except:
+    except Exception as e:
         # exc_type, exc_val, exc_tb = sys.exc_info()
         # print('exc_type=', exc_type)
         # print('exc_val=', exc_val)
         # print('exc_tb=', exc_tb)
         # # traceback.print_tb(exc_tb)
         # traceback.print_stack(f=traceback.extract_stack())
-        tbe = traceback.TracebackException(*sys.exc_info(), capture_locals=True)
-        print(list(tbe.format_exception_only()))
+        # tbe = traceback.TracebackException(*sys.exc_info(), capture_locals=True)
+        # print(list(tbe.format_exception_only()))
+        print(str(type(e).__name__))
+        print(str(e))
+        print(''.join(traceback.format_tb(e.__traceback__)))
 
     # with A():
     #     f(1)
