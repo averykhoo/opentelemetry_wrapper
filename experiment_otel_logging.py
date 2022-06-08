e9cfd85dea135070301023e99b53a4acf331e91f
Avery <averykhoo@gmail.com>
2022-06-08 16:52:10 +0800

    convert everything to async

--- experiment_otel_logging.py
+++ experiment_otel_logging.py
@@ -1,5 +1,7 @@
+import asyncio
 import logging
 import sys
+import time
 
 from asgiref.sync import async_to_sync
 from opentelemetry import trace
@@ -24,11 +26,14 @@ trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(ConsoleSpanExp
 tracer = trace.get_tracer(__name__)
 
 
-def bitshift(x: int, shift_by: int):
+async def bitshift(x: int, shift_by: int):
     """
     shifts left if positive, right if negative
+    uses async in order to test that logging works normally
     """
     with tracer.start_as_current_span('def-bitshift'):
+        await asyncio.sleep(0.01)
+
         assert isinstance(x, int)
         assert isinstance(shift_by, int)
 
@@ -47,9 +52,10 @@ async def multiply(multiplier: int, multiplicand: int):
     """
     very silly way to multiply things inspired by exponentiation-by-squaring
     constrained to only use the `bitshift` function and addition
-    uses async in order to test that logging works normally
     """
     with tracer.start_as_current_span('def-multiply'):
+        await asyncio.sleep(0.01)
+
         logging.info(f'multiply {multiplier=} by {multiplicand=}')
         _negative = multiplicand < 0
         multiplicand = abs(multiplicand)
@@ -60,11 +66,11 @@ async def multiply(multiplier: int, multiplicand: int):
 
         accumulator = 0
         while multiplicand:
-            _tmp, multiplicand = multiplicand, bitshift(multiplicand, -1)
-            if _tmp != bitshift(multiplicand, 1):
+            _tmp, multiplicand = multiplicand, await bitshift(multiplicand, -1)
+            if _tmp != await bitshift(multiplicand, 1):
                 accumulator += multiplier
             if multiplicand:
-                multiplier = bitshift(multiplier, 1)
+                multiplier = await bitshift(multiplier, 1)
 
         return accumulator if not _negative else -accumulator
 
@@ -75,11 +81,14 @@ async def square(x):
     uses warning instead of info just for testing
     """
     with tracer.start_as_current_span('def-square'):
+        await asyncio.sleep(0.01)
+
         assert isinstance(x, int)
         if x < 0:
             logging.warning(f'squaring absolute of negative integer abs({x=})')
         else:
             logging.info(f'squaring non-negative number {x=}')
+
         return await multiply(abs(x), abs(x))
 
 
@@ -90,6 +99,8 @@ async def exponentiate(base, exponent):
     constrained not to use any math other than the functions defined above
     """
     with tracer.start_as_current_span('def-multiply'):
+        await asyncio.sleep(0.01)
+
         logging.info(f'exponentiate {base=} by non-negative {exponent=}')
         assert exponent >= 0
         assert isinstance(base, int)
@@ -97,8 +108,8 @@ async def exponentiate(base, exponent):
 
         out = 1
         while exponent:
-            _tmp, exponent = exponent, bitshift(exponent, -1)
-            if _tmp != bitshift(exponent, 1):
+            _tmp, exponent = exponent, await bitshift(exponent, -1)
+            if _tmp != await bitshift(exponent, 1):
                 out = await multiply(out, base)
             if exponent:
                 base = await square(base)
@@ -106,4 +117,6 @@ async def exponentiate(base, exponent):
 
 
 if __name__ == '__main__':
+    t = time.perf_counter()
     assert exponentiate(-10, 5) == -100000
+    print(round(time.perf_counter() - t, 10))
