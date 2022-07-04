a0b902cb5d99bb68d5bf16ba5c909cb3ad95a0d6
Avery <averykhoo@gmail.com>
2022-07-04 11:11:00 +0800

    add todo for dataclasses

--- opentelemetry_wrapper/instrument_decorator.py
+++ opentelemetry_wrapper/instrument_decorator.py
@@ -170,6 +170,7 @@ def _instrument_class(cls: type,
         cls.__new__ = instrument_decorate(cls.__new__, func_name=f'{class_name}.__new__')
     if cls.__init__ is not object.__init__:
         cls.__init__ = instrument_decorate(cls.__init__, func_name=f'{class_name}.__init__')
+    # todo: also wrap __post_init__
 
     # also wrap the call method, if it exists
     if not isinstance(cls.__call__, type(object.__call__)):
