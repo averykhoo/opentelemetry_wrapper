74d4d52323333b077364323b2a450c4e30a4a88f
Avery <averykhoo@gmail.com>
2022-11-29 11:05:15 +0800

    try this way?

--- opentelemetry_wrapper/__init__.py
+++ opentelemetry_wrapper/__init__.py
@@ -2,12 +2,12 @@
 a wrapper around `opentelemetry` and `opentelemetry-instrumentation-*` to make life a bit easier
 """
 
-from opentelemetry_wrapper.config import __version__
 from opentelemetry_wrapper.instrument_dataclasses import instrument_dataclasses
 from opentelemetry_wrapper.instrument_decorator import instrument_decorate
 from opentelemetry_wrapper.instrument_fastapi import instrument_fastapi
 from opentelemetry_wrapper.instrument_logging import instrument_logging
 from opentelemetry_wrapper.instrument_requests import instrument_requests
+from .config import __version__
 
 
 @instrument_decorate
