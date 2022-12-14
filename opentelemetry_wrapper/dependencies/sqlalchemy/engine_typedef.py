99f5af4871b9029c831263af0bee7fdf2139efd1
Avery Khoo <averykhoo@gmail.com>
2022-12-14 11:43:14 +0800

    allow sqlalchemy to not exist

--- opentelemetry_wrapper/dependencies/sqlalchemy/engine_typedef.py
+++ opentelemetry_wrapper/dependencies/sqlalchemy/engine_typedef.py
@@ -1,7 +1,21 @@
 from typing import TypeVar
 
-from sqlalchemy.engine import Engine as LegacyEngine
-from sqlalchemy.ext.asyncio import AsyncEngine
-from sqlalchemy.future import Engine as FutureEngine
+try:
+    from sqlalchemy.engine import Engine as LegacyEngine
+except ImportError:
+    # ignore mypy here since we're only using this as a typedef
+    from typing import Any as LegacyEngine  # type: ignore[assignment] # flake8: noqa: F401
+
+try:
+    from sqlalchemy.ext.asyncio import AsyncEngine
+except ImportError:
+    # ignore mypy here since we're only using this as a typedef
+    from typing import Any as AsyncEngine  # type: ignore[assignment] # flake8: noqa: F401
+
+try:
+    from sqlalchemy.future import Engine as FutureEngine
+except ImportError:
+    # ignore mypy here since we're only using this as a typedef
+    from typing import Any as FutureEngine  # type: ignore[assignment] # flake8: noqa: F401
 
 AnyEngine = TypeVar('AnyEngine', LegacyEngine, FutureEngine, AsyncEngine)
