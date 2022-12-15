174ec446d0c7c583161524ea78f4d205df394fe0
Avery Khoo <averykhoo@gmail.com>
2022-12-15 18:34:07 +0800

    add mor error checks

--- opentelemetry_wrapper/config/otel_exporter_otlp.py
+++ opentelemetry_wrapper/config/otel_exporter_otlp.py
@@ -7,6 +7,8 @@ from typing import Tuple
 
 VALID_HTTP_HEADER_CHARS = ''.join(string.printable.split()) + ' '
 REGEX_HTTP_HEADER = re.compile(f'[{re.escape(VALID_HTTP_HEADER_CHARS)}]+')
+VALID_HTTP_HEADER_NAME_CHARS = string.digits + string.ascii_letters
+REGEX_HTTP_HEADER_NAME = re.compile(f'[{re.escape(VALID_HTTP_HEADER_NAME_CHARS)}]+')
 
 
 def getenv_otel_exporter_otlp_endpoint() -> str:
@@ -22,12 +24,21 @@ def getenv_otel_exporter_otlp_endpoint() -> str:
 
 def getenv_otel_exporter_otlp_header() -> Tuple[Tuple[str, str], ...]:
     out = []
+    seen = set()
     for header in REGEX_HTTP_HEADER.findall(os.getenv('OTEL_EXPORTER_OTLP_HEADER', '')):
         header_name, _, header_value = header.partition('=')
-        if _:
-            out.append((header_name, header_value))
+        if not _:
+            warnings.warn(f'invalid `OTEL_EXPORTER_OTLP_HEADER` key=value pair (missing "=" delimiter): '
+                          f'"{header[5:]}..."')  # truncate in case it's sensitive
+        elif REGEX_HTTP_HEADER_NAME.fullmatch(header_name) is None:
+            warnings.warn(f'invalid header name: {header_name}')  # value may be sensitive
         else:
-            warnings.warn(f'invalid `OTEL_EXPORTER_OTLP_HEADER` key=value pair (missing "=" delimiter): "{header}"')
+            out.append((header_name, header_value))
+
+            # check for duplicates, case insensitive pure ascii
+            if header_name.lower() in seen:
+                warnings.warn(f'duplicate header {header_name}')
+            seen.add(header_name.lower())
 
     # despite having a type signature of Sequence[Tuple[str, str]], opentelemetry does not accept a list of tuples
     # if you try to feed it that, it crashes on startup
