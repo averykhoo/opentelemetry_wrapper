2477b65b64855cbcbc184a5b6ae10000d2d5c2de
Avery Khoo <averykhoo@gmail.com>
2023-02-08 17:19:23 +0800

    fix lint

--- opentelemetry_wrapper/dependencies/fastapi/fastapi_typedef.py
+++ opentelemetry_wrapper/dependencies/fastapi/fastapi_typedef.py
@@ -1,13 +1,15 @@
 from typing import Any
 
+# note: ignore E303 too many blank lines since flak8 disagrees with pycharm
 try:
     from fastapi import FastAPI
 
 
-    def is_fastapi_app(item: Any) -> bool:
+    def is_fastapi_app(item: Any) -> bool:  # noqa: E303
         return isinstance(item, FastAPI)
 
 
+    # noqa: E303
     FastApiType = FastAPI
 
 except ImportError:
@@ -15,5 +17,6 @@ except ImportError:
         return False
 
 
+    # noqa: E303
     # ignore mypy here since we're only using this as a typedef
     FastApiType = Any  # type: ignore[assignment,misc]
--- opentelemetry_wrapper/dependencies/sqlalchemy/engine_typedef.py
+++ opentelemetry_wrapper/dependencies/sqlalchemy/engine_typedef.py
@@ -1,21 +1,22 @@
 from typing import Any
 from typing import TypeVar
 
+# note: ignore E303 too many blank lines since flak8 disagrees with pycharm
 try:
     from sqlalchemy.engine import Engine as LegacyEngine
     from sqlalchemy.ext.asyncio import AsyncEngine
     from sqlalchemy.future import Engine as FutureEngine
 
 
-    def is_sqlalchemy_engine(item: Any) -> bool:
+    def is_sqlalchemy_engine(item: Any) -> bool:  # noqa: E303
         return isinstance(item, (LegacyEngine, AsyncEngine, FutureEngine))
 
 
-    def is_sqlalchemy_sync_engine(item: Any) -> bool:
+    def is_sqlalchemy_sync_engine(item: Any) -> bool:  # noqa: E303
         return isinstance(item, (LegacyEngine, FutureEngine))
 
 
-    def is_sqlalchemy_async_engine(item: Any) -> bool:
+    def is_sqlalchemy_async_engine(item: Any) -> bool:  # noqa: E303
         # noinspection PyTypeHints
         return isinstance(item, AsyncEngine)
 
@@ -24,8 +25,9 @@ except ImportError:
         return False
 
 
-    LegacyEngine = Any
-    AsyncEngine = Any
-    FutureEngine = Any
+    # noqa: E303
+    LegacyEngine = Any  # type: ignore[assignment,misc]
+    AsyncEngine = Any  # type: ignore[assignment,misc]
+    FutureEngine = Any  # type: ignore[assignment,misc]
 
 AnyEngine = TypeVar('AnyEngine', LegacyEngine, FutureEngine, AsyncEngine)
