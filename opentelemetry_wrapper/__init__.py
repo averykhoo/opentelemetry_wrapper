f03f14c85c4fa365553c01edaaf03bb5e6e5d168
khoo.ys <khoo.ys@xtraman.org>
2023-11-07 09:36:16 +0800

    bump version

--- opentelemetry_wrapper/__init__.py
+++ opentelemetry_wrapper/__init__.py
@@ -2,7 +2,7 @@
 a wrapper around `opentelemetry` and `opentelemetry-instrumentation-*` to make life a bit easier
 """
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 from opentelemetry_wrapper.config.otel_headers import OTEL_WRAPPER_DISABLED
 from opentelemetry_wrapper.dependencies.opentelemetry.instrument_dataclasses import instrument_dataclasses
