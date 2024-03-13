05cd7f43ab2a5ce70b26ed0ac331ddc95e78257e
khoo.ys <khoo.ys@xtraman.org>
2024-03-13 17:18:35 +0800

    also test logging a basemodel

--- log_weird_things.py
+++ log_weird_things.py
@@ -1,5 +1,7 @@
 import logging
 
+from pydantic import BaseModel
+
 from opentelemetry_wrapper.dependencies.opentelemetry.instrument_logging import instrument_logging
 
 instrument_logging()
@@ -10,3 +12,10 @@ logging.warning([1, 2, 3])
 logging.warning({1: 11, None: None, 'a': 'A'})
 logging.warning(object())
 logging.warning(1.234 + 4j)
+
+
+class Something(BaseModel):
+    key: str = 'value'
+
+
+logging.warning(Something())
