59b343f3eda07871cca1cbc0ecb907932b65baa8
khoo.ys <khoo.ys@xtraman.org>
2024-04-08 17:36:31 +0800

    oops blank is fine too

--- opentelemetry_wrapper/config/otel_wrapper_prometheus_exporter.py
+++ opentelemetry_wrapper/config/otel_wrapper_prometheus_exporter.py
@@ -6,6 +6,9 @@ from typing import Optional
 def get_prometheus_port() -> Optional[int]:
     out = os.getenv('OTEL_EXPORTER_PROMETHEUS_PORT', '').strip()
 
+    if not out:
+        return None
+
     if not out.isdigit():
         warnings.warn(f'`OTEL_EXPORTER_PROMETHEUS_PORT` is non-numeric, and will be ignored: {out!r}')
         return None
