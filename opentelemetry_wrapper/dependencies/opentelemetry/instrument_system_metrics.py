b8c52a874ef189e7e9df617922469b90cf2fa3cc
khoo.ys <khoo.ys@xtraman.org>
2024-04-09 14:23:45 +0800

    update this to just show all metrics

--- opentelemetry_wrapper/dependencies/opentelemetry/instrument_system_metrics.py
+++ opentelemetry_wrapper/dependencies/opentelemetry/instrument_system_metrics.py
@@ -7,30 +7,7 @@ from opentelemetry_wrapper.dependencies.opentelemetry.otel_providers import init
 
 @instrument_decorate
 def instrument_system_metrics():
-    # no-op
     if OTEL_WRAPPER_DISABLED:
         return
-
-    # init metrics
     init_meter_provider()
-
-    # configure all available metrics
-    SystemMetricsInstrumentor(config={
-        "system.cpu.time":                ["idle", "user", "system", "irq"],
-        "system.cpu.utilization":         ["idle", "user", "system", "irq"],
-        "system.memory.usage":            ["used", "free", "cached"],
-        "system.memory.utilization":      ["used", "free", "cached"],
-        "system.swap.usage":              ["used", "free"],
-        "system.swap.utilization":        ["used", "free"],
-        "system.disk.io":                 ["read", "write"],
-        "system.disk.operations":         ["read", "write"],
-        "system.disk.time":               ["read", "write"],
-        "system.network.dropped.packets": ["transmit", "receive"],
-        "system.network.packets":         ["transmit", "receive"],
-        "system.network.errors":          ["transmit", "receive"],
-        "system.network.io":              ["transmit", "receive"],
-        "system.network.connections":     ["family", "type"],
-        "system.thread_count":            None,  # why is this None? does it even exist? it's in the docs though
-        "process.runtime.memory":         ["rss", "vms"],
-        "process.runtime.cpu.time":       ["user", "system"],
-    }).instrument()
+    SystemMetricsInstrumentor().instrument()
