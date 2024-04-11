7a1b77c31de3d459ad7ed311a3d498aac6c8ba10
khoo.ys <khoo.ys@xtraman.org>
2024-04-11 17:41:03 +0800

    add whitelisted response headers

--- opentelemetry_wrapper/dependencies/opentelemetry/instrument_requests.py
+++ opentelemetry_wrapper/dependencies/opentelemetry/instrument_requests.py
@@ -1,8 +1,26 @@
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.trace import Span
+from requests import PreparedRequest
+from requests import Response
 
 from opentelemetry_wrapper.config.otel_headers import OTEL_WRAPPER_DISABLED
 from opentelemetry_wrapper.dependencies.opentelemetry.instrument_decorator import instrument_decorate
 from opentelemetry_wrapper.dependencies.opentelemetry.otel_providers import init_meter_provider
+
+# TODO: don't hardcode!
+_HEADERS = [  # this should all be lowercase!
+    'x-kong-proxy-latency',
+    'x-kong-upstream-latency',
+    'x-envoy-upstream-service-time',
+    'x-authz-response-time-seconds',
+    'x-process-time-seconds',  # for testing, defined in fastapi_main.py
+]
+
+
+def response_hook(span: Span, _request: PreparedRequest, result: Response) -> None:
+    for header_name in _HEADERS:
+        if header_name in result.headers:
+            span.set_attribute(f'http.response.header.{header_name}', result.headers[header_name])
 
 
 @instrument_decorate
@@ -22,4 +40,4 @@ def instrument_requests():
 
     _instrumentor = RequestsInstrumentor()
     if not _instrumentor.is_instrumented_by_opentelemetry:
-        _instrumentor.instrument()
+        _instrumentor.instrument(response_hook=response_hook)
