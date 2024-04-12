2099a32363febd3a6cacab86246ec5b400bba84a
khoo.ys <khoo.ys@xtraman.org>
2024-04-12 18:42:34 +0800

    remove sqlalchemy stubs

--- opentelemetry_wrapper/__init__.py
+++ opentelemetry_wrapper/__init__.py
@@ -2,7 +2,7 @@
 a wrapper around `opentelemetry` and `opentelemetry-instrumentation-*` to make life a bit easier
 """
 
-__version__ = '0.1.9'
+__version__ = '0.1.10'
 
 from opentelemetry_wrapper.config.otel_headers import OTEL_WRAPPER_DISABLED
 from opentelemetry_wrapper.dependencies.opentelemetry.instrument_dataclasses import instrument_dataclasses
