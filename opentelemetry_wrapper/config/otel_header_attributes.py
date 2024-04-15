3c04c14974e6762141fb4ba6fa9a163ad2d037d9
khoo.ys <khoo.ys@xtraman.org>
2024-04-15 15:07:11 +0800

    only load and parse x-userinfo

--- opentelemetry_wrapper/config/otel_header_attributes.py
+++ opentelemetry_wrapper/config/otel_header_attributes.py
@@ -3,32 +3,7 @@ import re
 from functools import lru_cache
 from typing import List
 
-# todo: don't hardcode this because it's not nice to do that to random people trying out a published library
-_DEFAULT_HEADER_ATTRIBUTES = [
-    # headers set by the user's browser
-    # 'user-agent',
-    # 'cookie',
-
-    # headers set by OPA policy
-    'x-client-id',
-    'x-preferred-username',
-    # 'x-full-name',
-    # 'x-given-name',
-    # 'x-family-name',
-    'x-resource-access',
-    # 'x-realm-roles',
-    # 'x-groups',
-
-    # headers set by Kong
-    # 'authorization',
-    'x-userinfo',
-    # 'x-request-id',
-
-    #  headers set by K8s
-    # 'x-real-ip',
-    # 'x-forwarded-for',
-    # 'x-original-forwarded-for',
-]
+_DEFAULT_HEADER_ATTRIBUTES = ['x-userinfo']
 
 # https://developers.cloudflare.com/rules/transform/request-header-modification/reference/header-format/
 REGEX_HEADER = re.compile(r'[a-z0-9_-]+', flags=re.I)
