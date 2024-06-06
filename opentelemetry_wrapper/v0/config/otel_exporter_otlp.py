9eda42bf8866519557dc841fce4052d5996d8ba4
khoo.ys <khoo.ys@xtraman.org>
2024-06-06 18:18:55 +0800

    make multiple otel headers easier via OTEL_EXPORTER_OTLP_HEADER_SEPARATOR

--- opentelemetry_wrapper/v0/config/otel_exporter_otlp.py
+++ opentelemetry_wrapper/v0/config/otel_exporter_otlp.py
@@ -15,6 +15,16 @@ REGEX_HTTP_HEADER_NAME = re.compile(f'[{re.escape(VALID_HTTP_HEADER_NAME_CHARS)}
 
 def getenv_otel_exporter_otlp_endpoint() -> str:
     """
+    >>> os.environ['OTEL_EXPORTER_OTLP_ENDPOINT'] = 'https://otel-collector.domain/some-endpoint'
+    >>> getenv_otel_exporter_otlp_endpoint()
+    'https://otel-collector.domain/some-endpoint'
+    >>> os.environ['OTEL_EXPORTER_OTLP_ENDPOINT'] = 'otel-collector/some-endpoint'  # could be pod-local
+    >>> getenv_otel_exporter_otlp_endpoint()
+    'otel-collector/some-endpoint'
+    >>> os.environ['OTEL_EXPORTER_OTLP_ENDPOINT'] = '/otel-collector'  # maybe it's a file? should i raise an error?
+    >>> getenv_otel_exporter_otlp_endpoint()
+    '/otel-collector'
+
     get oltp endpoint env var, or an empty string otherwise
     :return: url or empty string
     """
@@ -48,15 +58,33 @@ def getenv_otel_exporter_otlp_endpoint() -> str:
 
 
 def getenv_otel_exporter_otlp_header() -> Tuple[Tuple[str, str], ...]:
+    """
+    >>> os.environ['OTEL_EXPORTER_OTLP_HEADER'] = 'some-header=some-value'
+    >>> getenv_otel_exporter_otlp_header()
+    (('some-header', 'some-value'),)
+    >>> os.environ['OTEL_EXPORTER_OTLP_HEADER'] = 'a=b\\tc=d'
+    >>> getenv_otel_exporter_otlp_header()
+    (('a', 'b'), ('c', 'd'))
+    >>> os.environ['OTEL_EXPORTER_OTLP_HEADER_SEPARATOR'] = ';'
+    >>> os.environ['OTEL_EXPORTER_OTLP_HEADER'] = 'a=1;b=2;c=3'
+    >>> getenv_otel_exporter_otlp_header()
+    (('a', '1'), ('b', '2'), ('c', '3'))
+
+    :return:
+    """
     out = []
     seen = set()
-    for header in REGEX_HTTP_HEADER.findall(os.getenv('OTEL_EXPORTER_OTLP_HEADER', '')):
+    header_separator = os.getenv('OTEL_EXPORTER_OTLP_HEADER_SEPARATOR', '\t')
+    split_headers = os.getenv('OTEL_EXPORTER_OTLP_HEADER', '').split(header_separator)
+    for header in split_headers:
         header_name, _, header_value = header.partition('=')
         if not _:
             _header = f'"{header[:5]}..."' if len(header) > 8 else f'"{header}"'  # truncate long header pairs
             warnings.warn(f'invalid `OTEL_EXPORTER_OTLP_HEADER` key=value pair (missing "=" delimiter): {_header}')
         elif REGEX_HTTP_HEADER_NAME.fullmatch(header_name) is None:
             warnings.warn(f'invalid `OTEL_EXPORTER_OTLP_HEADER` header name: {header_name}')  # value may be sensitive
+        elif REGEX_HTTP_HEADER.fullmatch(header_value) is None:
+            warnings.warn(f'invalid `OTEL_EXPORTER_OTLP_HEADER` value for header_name: {header}')
         else:
             out.append((header_name, header_value))
 
@@ -76,6 +104,24 @@ def getenv_otel_exporter_otlp_insecure() -> Optional[bool]:
     check if OTEL_EXPORTER_OTLP_INSECURE=True
     defaults to secure (i.e. `False`) if invalid
     returns None if unset, since that's falsy
+
+    >>> getenv_otel_exporter_otlp_insecure() # returns None
+
+    >>> os.environ['OTEL_EXPORTER_OTLP_INSECURE'] = 'invalid' # prints a warning
+    >>> getenv_otel_exporter_otlp_insecure()
+    False
+    >>> os.environ['OTEL_EXPORTER_OTLP_INSECURE'] = 'false'
+    >>> getenv_otel_exporter_otlp_insecure()
+    False
+    >>> os.environ['OTEL_EXPORTER_OTLP_INSECURE'] = 'true'
+    >>> getenv_otel_exporter_otlp_insecure()
+    True
+    >>> os.environ['OTEL_EXPORTER_OTLP_INSECURE'] = 'TRUE'
+    >>> getenv_otel_exporter_otlp_insecure()
+    True
+    >>> os.environ['OTEL_EXPORTER_OTLP_INSECURE'] = 'tRuE'
+    >>> getenv_otel_exporter_otlp_insecure()
+    True
 
     :return: True if insecure; False if secure; None if unset
     """
